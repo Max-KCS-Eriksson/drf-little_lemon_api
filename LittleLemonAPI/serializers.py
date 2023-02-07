42cb0a8f3715b5d12116b8f1e6dd7145687bf430
Max <teratoscincus@hotmail.com>
2023-02-07 15:18:29 +0100

    Create UserSerializer

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,5 +1,13 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+
 from .models import Category, MenuItem, Cart, Order, OrderItem
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username"]
 
 
 class CategorySerializer(serializers.ModelSerializer):

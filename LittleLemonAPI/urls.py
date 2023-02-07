883c27cc82f759c0c96ceba77bb914da70166826
Max <teratoscincus@hotmail.com>
2023-02-07 15:57:55 +0100

    Add endpoint for listing and makeing user manager

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 app_name = "LittleLemonAPI"
 urlpatterns = [
+    # Menu-items endpoints.
     path("menu-items", views.MenuItemsView.as_view()),
     path("menu-items/<int:pk>", views.SingleMenuItemView.as_view()),
+    # User role management endpoint.
+    path("groups/managers/users", views.ManagersView.as_view()),
 ]
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
+from django.contrib.auth.models import User, Group
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import MenuItem
-from .serializers import MenuItemSerializer
+from .serializers import MenuItemSerializer, UserSerializer
 
 
 def is_manager(request):
@@ -52,3 +54,42 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
         menu_item = self.get_object()
         menu_item.delete()
         return Response(status=status.HTTP_200_OK)
+
+
+class ManagersView(generics.ListCreateAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        if self.request.method == "GET":
+            # Manager only for GET requests.
+            return User.objects.filter(groups__name="Manager")
+        else:
+            # All users for POST requests to allow assignment as Manager.
+            return User.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        # Only allow request from managers.
+        if not is_manager(request):
+            return Response(
+                {"message": "You are not authorized"}, status.HTTP_403_FORBIDDEN
+            )
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        # Only allow request from managers.
+        if not is_manager(request):
+            return Response(
+                {"message": "You are not authorized"}, status.HTTP_403_FORBIDDEN
+            )
+
+        # Get user or 404.
+        try:
+            user = User.objects.get(username=request.POST.get("username"))
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        # Assign user to group.
+        manager_group = Group.objects.get(name="Manager")
+        manager_group.user_set.add(user)
+        return Response(status=status.HTTP_201_CREATED)
+        # return super().post(request, *args, **kwargs)

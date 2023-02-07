65d65a4caa12f482e95e4830d465881561708936
Max <teratoscincus@hotmail.com>
2023-02-07 13:11:51 +0100

    Register models with admin panel

--- LittleLemonAPI/admin.py
+++ LittleLemonAPI/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, MenuItem, Cart, Order, OrderItem
+
+admin.site.register([Category, MenuItem, Cart, Order, OrderItem])

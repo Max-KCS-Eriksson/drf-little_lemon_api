de6cad247753258121392cd652bb1e23c0d21ca8
Max <teratoscincus@hotmail.com>
2023-02-08 14:22:55 +0100

    Remove price column from Cart as it is calculated
    Price is calculated in the serializer.

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -36,7 +36,8 @@ class Cart(models.Model):
     menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.SmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    # Not needed as it is calculated in the serializer.
+    # price = models.DecimalField(max_digits=6, decimal_places=2)
 
     class Meta:
         unique_together = ("menuitem", "user")  # One entry per menuitem in a cart.

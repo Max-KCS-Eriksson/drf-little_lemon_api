67a5cc756f30e49d837a21433cfd53af70760eb5
Max <teratoscincus@hotmail.com>
2023-02-08 10:27:08 +0100

    Set default permission class to IsAuthenticated

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -139,6 +139,9 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
 
 # Djoser settings

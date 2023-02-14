37a8a8aa3e64f0826f01d135940b0315babd0d94
Max <teratoscincus@hotmail.com>
2023-02-14 12:08:11 +0100

    Reduce items per page

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -150,7 +150,7 @@ REST_FRAMEWORK = {
         "rest_framework.filters.SearchFilter",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 2,
     # Throttling
     "DEFAULT_THROTTLE_CLASSES": [
         "rest_framework.throttling.AnonRateThrottle",
